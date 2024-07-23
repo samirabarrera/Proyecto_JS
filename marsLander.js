469f6b7d80783f01b0008c7e0cc31ef7287cb094
Samira Barrera <samira.barrera@galileo.edu>
2024-07-22 23:45:47 -0600

    episodio dos a uno

--- marsLander.js
+++ marsLander.js
@@ -1,5 +1,10 @@
-const N = parseInt(readline()); // the number of points used to draw the surface of Mars.
-for (let i = 0; i < N; i++) {
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+
+const surfaceN = parseInt(readline()); // the number of points used to draw the surface of Mars.
+for (let i = 0; i < surfaceN; i++) {
     var inputs = readline().split(' ');
     const landX = parseInt(inputs[0]); // X coordinate of a surface point. (0 to 6999)
     const landY = parseInt(inputs[1]); // Y coordinate of a surface point. By linking all the points together in a sequential fashion, you form the surface of Mars.
@@ -10,21 +15,20 @@ while (true) {
     var inputs = readline().split(' ');
     const X = parseInt(inputs[0]);
     const Y = parseInt(inputs[1]);
-    const highSpeed = parseInt(inputs[2]); // the horizontal speed (in m/s), can be negative.
+    const hSpeed = parseInt(inputs[2]); // the horizontal speed (in m/s), can be negative.
     const vSpeed = parseInt(inputs[3]); // the vertical speed (in m/s), can be negative.
     const fuel = parseInt(inputs[4]); // the quantity of remaining fuel in liters.
     const rotate = parseInt(inputs[5]); // the rotation angle in degrees (-90 to 90).
     const power = parseInt(inputs[6]); // the thrust power (0 to 4).
 
-    let thrustPower = 0;
-    let rotateAngle = 0;
-
-
+    // Write an action using console.log()
+    // To debug: console.error('Debug messages...');
     if (vSpeed <= -40) {
-        console.log ('0 4');
-    } else if (vSpeed > -40) {
-        console.log ('0 0');
-    } else if () {
-        console.log
-    }
+        console.log('0 4'); // thrust power 4 if vertical speed <= -40
+    } else {
+        console.log('0 0'); // otherwise thrust power 0
+    }   
+
+    // 2 integers: rotate power. rotate is the desired rotation angle (should be 0 for level 1), power is the desired thrust power (0 to 4).
+    console.log('0 3');
 }
